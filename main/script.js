e21328a2babee16a8ed8b4ca8e05f2d336a2645f
Magomed <bacarovmagomed9@gmail.com>
2025-01-24 00:38:39 +0300

    fix:linked the button to the form

--- main/script.js
+++ main/script.js
@@ -1,14 +1,19 @@
 const buttonPlus = document.querySelector('.plus');
 let isRotated = false;
+const formContainer = document.querySelector('.form-container')
 
 buttonPlus.addEventListener('click', () => {
     if (!isRotated) {
+         formContainer.style.transform = 'scale(1)'
+        formContainer.style.display = 'block';
         buttonPlus.style.transform = 'rotate(45deg)';
         buttonPlus.style.backgroundColor = 'red';
         isRotated = true;
     } else {
+        formContainer.style.transform = 'scale(0)';
         buttonPlus.style.transform = 'rotate(0deg)'; 
         buttonPlus.style.backgroundColor = 'rgb(128, 0, 255)';
         isRotated = false; 
     }
 });
+
