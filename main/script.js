9495a9bf4452e3435c966294061b6b2235192ac5
Magomed <bacarovmagomed9@gmail.com>
2025-01-24 01:17:48 +0300

    upd: gitkeep file

--- main/script.js
+++ main/script.js
@@ -5,12 +5,14 @@ const formContainer = document.querySelector('.form-container')
 buttonPlus.addEventListener('click', () => {
     if (!isRotated) {
          formContainer.style.transform = 'scale(1)'
+         formContainer.style.transition = '1s';
         formContainer.style.display = 'block';
         buttonPlus.style.transform = 'rotate(45deg)';
         buttonPlus.style.backgroundColor = 'red';
         isRotated = true;
     } else {
         formContainer.style.transform = 'scale(0)';
+        formContainer.style.transition = '0.5s';
         buttonPlus.style.transform = 'rotate(0deg)'; 
         buttonPlus.style.backgroundColor = 'rgb(128, 0, 255)';
         isRotated = false; 
