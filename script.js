6f0919b3ffb43f59677bdaf12071567a6ec44458
Magomed <bacarovmagomed9@gmail.com>
2025-01-23 14:58:17 +0300

    commit

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ let isRotated = false;
 buttonPlus.addEventListener('click', () => {
     if (!isRotated) {
         buttonPlus.style.transform = 'rotate(45deg)';
+        buttonPlus.style.backgroundColor = 'red';
         isRotated = true;
     } else {
         buttonPlus.style.transform = 'rotate(0deg)'; 
+        buttonPlus.style.backgroundColor = 'rgb(128, 0, 255)';
         isRotated = false; 
     }
 });
